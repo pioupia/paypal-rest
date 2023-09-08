70796d13f82f9b8b293c654288d7cbb41759e10c
Pioupia <pioupiadeux@gmail.com>
2023-09-08 22:14:10 +0200

    Feat (Auth): Create the Auth function

---
+++ src/Manager/AuthManager.ts
@@ -0,0 +1,29 @@
+import { getConfig, setAccessToken } from "./ConfigManager";
+import PaypalTSError, { configError } from "./Errors";
+
+export default function auth(): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+        const { client_id, client_secret, base_url } = getConfig();
+
+        if (!client_id || !client_secret)
+            return reject(configError());
+
+        try {
+            const auth = Buffer.from(client_id + ":" + client_secret)
+                .toString("base64");
+            const response = await fetch(`${base_url}/v1/oauth2/token`, {
+                method: "post",
+                body: "grant_type=client_credentials",
+                headers: {
+                    Authorization: 'Basic ' + auth,
+                },
+            });
+
+            const data = await response.json();
+            setAccessToken(data.access_token);
+            return resolve(true);
+        } catch (e) {
+            throw new PaypalTSError("An error has occurred during the auth with PayPal. Error:" + e);
+        }
+    });
+}
