9fb83e9f51bc0851f78f22f1faa34c8e64a44223
Pioupia <pioupiadeux@gmail.com>
2023-09-21 13:03:10 +0200

    Feat (Subscription): Create the `createSubscription` function

--- src/types/Subscriptions.ts
+++ src/types/Subscriptions.ts
@@ -3,6 +3,8 @@ import PaymentPreferencesBuilder from "../Builders/PaymentPreferencesBuilder";
 import { PaymentPreferencesProps } from "./PaymentPreferences";
 import { BillingCycleProps } from "./BillingCycle";
 import ProductBuilder from "../Builders/ProductBuilder";
+import { ProductUpdateBuilder } from "../Builders/ProductUpdateBuilder";
+import { LinksData } from "./index";
 
 export type SubscriptionsStatus = "ACTIVE" | "INACTIVE" | "CREATED";
 
@@ -14,7 +16,7 @@ export interface Taxes {
 export type JSONTaxes = Omit<Taxes, 'percentage'> & { percentage: string };
 
 export interface SubscriptionsBuilderProps<T extends 'Props' | 'JSON' = 'Props'> {
-    product_id: T extends 'Props' ? string | ProductBuilder : string;
+    product_id: T extends 'Props' ? string | ProductBuilder | ProductUpdateBuilder : string;
     name: string;
     billing_cycles: T extends 'Props' ? (BillingCycleBuilder | BillingCycleProps)[] : (BillingCycleProps<T>)[];
     payment_preferences: T extends 'Props' ? (PaymentPreferencesBuilder | PaymentPreferencesProps) :  PaymentPreferencesProps<T>;
@@ -22,4 +24,16 @@ export interface SubscriptionsBuilderProps<T extends 'Props' | 'JSON' = 'Props'>
     description?: string;
     quantity_supported?: boolean;
     taxes?: T extends 'Props' ? Taxes : JSONTaxes;
+}
+
+export interface SubscriptionsJSON {
+    id: string;
+    product_id: string;
+    name: string;
+    status: SubscriptionsStatus;
+    usage_type: "LICENSED" | "UNLIMITED";
+    create_time: string;
+    links?: LinksData[];
+    update_time?: string;
+    description?: string;
 }
