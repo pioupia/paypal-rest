1495a9eb3f1787b22b699da4cce74256b1dadf23
pioupia <49518790+pioupia@users.noreply.github.com>
2023-09-25 19:19:18 +0200

    Revert "Feat (Builders): Create the SubscriptionInlinePlanBuilder"

--- src/Builders/SubscriptionPlanBuilder.ts
+++ src/Builders/SubscriptionPlanBuilder.ts
@@ -2,33 +2,43 @@ import {
     JSONTaxes,
     SubscriptionsPlanBuilderProps,
     SubscriptionsPlanJSON,
-    SubscriptionsStatus
+    SubscriptionsStatus,
+    Taxes
 } from "../types/Subscriptions";
 import ProductBuilder from "./ProductBuilder";
+import BillingCycleBuilder from "./BillingCycleBuilder";
+import PaymentPreferencesBuilder from "./PaymentPreferencesBuilder";
 import PaypalTSError from "../Manager/Errors";
+import { BillingCycleProps } from "../types/BillingCycle";
+import { PaymentPreferencesProps } from "../types/PaymentPreferences";
 import { ProductUpdateBuilder } from "./ProductUpdateBuilder";
 import { createSubscriptionPlan } from "../Functions/Subscriptions";
-import SubscriptionInlinePlanBuilder from "./SubscriptionInlinePlanBuilder";
 
 const ALLOWS_STATUS = ["ACTIVE", "INACTIVE", "CREATED"];
 
-export default class SubscriptionPlanBuilder extends SubscriptionInlinePlanBuilder {
+export default class SubscriptionPlanBuilder {
     private product_id?: string;
     private name?: string;
+    private billing_cycles: BillingCycleBuilder[];
+    private payment_preferences?: PaymentPreferencesBuilder;
     private status?: SubscriptionsStatus;
     private description?: string;
     private quantity_supported?: boolean;
+    private taxes?: Taxes;
 
     constructor(data?: Partial<SubscriptionsPlanBuilderProps>) {
-        super(data);
-
         if (data?.product_id) {
             this.product_id = (data.product_id instanceof ProductBuilder) || (data.product_id instanceof ProductUpdateBuilder) ? data.product_id.toJSON().id : data.product_id;
         }
         this.name = data?.name;
+        this.billing_cycles = data?.billing_cycles?.map(billingCycle => {
+            return (billingCycle instanceof BillingCycleBuilder) ? billingCycle : new BillingCycleBuilder(billingCycle);
+        }) || [];
+        this.payment_preferences = (data?.payment_preferences instanceof PaymentPreferencesBuilder) ? data.payment_preferences : new PaymentPreferencesBuilder(data?.payment_preferences);
         this.status = data?.status;
         this.description = data?.description;
         this.quantity_supported = data?.quantity_supported;
+        this.taxes = data?.taxes;
     }
 
     public setProductId(product_id: string | ProductBuilder | ProductUpdateBuilder): SubscriptionPlanBuilder {
@@ -69,6 +79,36 @@ export default class SubscriptionPlanBuilder extends SubscriptionInlinePlanBuild
         return this;
     }
 
+    public setTaxes(taxes: Taxes): SubscriptionPlanBuilder {
+        this.taxes = taxes;
+        return this;
+    }
+
+    public addBillingCycle(...billing_cycle: (BillingCycleBuilder | BillingCycleProps)[]): SubscriptionPlanBuilder {
+        if (billing_cycle.length + this.billing_cycles.length > 12)
+            throw new PaypalTSError("Invalid billing_cycle the length must be less than 12.");
+
+        this.billing_cycles.push(...billing_cycle.map(billingCycle => {
+            return (billingCycle instanceof BillingCycleBuilder) ? billingCycle : new BillingCycleBuilder(billingCycle);
+        }));
+        return this;
+    }
+
+    public setBillingCycles(...billing_cycles: (BillingCycleBuilder | BillingCycleProps)[]): SubscriptionPlanBuilder {
+        if (billing_cycles.length > 12)
+            throw new PaypalTSError("Invalid billing_cycle the length must be less than 12.");
+
+        this.billing_cycles = billing_cycles.map(billingCycle => {
+            return (billingCycle instanceof BillingCycleBuilder) ? billingCycle : new BillingCycleBuilder(billingCycle);
+        });
+        return this;
+    }
+
+    public setPaymentPreferences(payment_preferences: PaymentPreferencesBuilder | PaymentPreferencesProps): SubscriptionPlanBuilder {
+        this.payment_preferences = (payment_preferences instanceof PaymentPreferencesBuilder) ? payment_preferences : new PaymentPreferencesBuilder(payment_preferences);
+        return this;
+    }
+
     public create(): Promise<SubscriptionsPlanJSON> {
         return new Promise(async (resolve, reject) => {
             createSubscriptionPlan(this.toJSON()).then(resolve).catch(reject);
@@ -77,33 +117,36 @@ export default class SubscriptionPlanBuilder extends SubscriptionInlinePlanBuild
 
     public toJSON(): Readonly<SubscriptionsPlanBuilderProps<'JSON'>> {
         this.validate();
-
         return {
             product_id: this.product_id!,
             name: this.name!,
             status: this.status,
-            billing_cycles: super.billing_cycles.map(billingCycle => {
+            billing_cycles: this.billing_cycles.map(billingCycle => {
                 return billingCycle.toJSON();
             }),
-            payment_preferences: super.payment_preferences!.toJSON(),
+            payment_preferences: this.payment_preferences!.toJSON(),
             description: this.description,
             quantity_supported: this.quantity_supported,
-            taxes: super.taxes as JSONTaxes | undefined
+            taxes: this.taxes as JSONTaxes | undefined
         }
     }
 
-    protected validate(): void {
+    private validate(): void {
         if (!this.product_id?.length || this.product_id.length < 6 || this.product_id.length > 50)
             throw new PaypalTSError("Invalid product_id the length must be between 6 and 50 characters.");
         if (!this.name?.length || this.name.length > 127)
             throw new PaypalTSError("Invalid name the length must be between 1 and 127 characters.");
-
-        super.validate();
-
-        if (!super.billing_cycles.length)
+        if (!this.billing_cycles.length)
             throw new PaypalTSError("billing_cycle are required.");
-        if (!super.payment_preferences)
+        if (this.billing_cycles.length > 12)
+            throw new PaypalTSError("Invalid billing_cycle the length must be less than 12.");
+        if (!this.payment_preferences)
             throw new PaypalTSError("payment_preferences are required.");
+        this.payment_preferences.toJSON();
+
+        this.billing_cycles.forEach(billingCycle => {
+            billingCycle.toJSON();
+        });
 
         if (this.status && !ALLOWS_STATUS.includes(this.status))
             throw new PaypalTSError(`Invalid status the status must be one of the following: ${ALLOWS_STATUS.join(", ")}`);
@@ -111,5 +154,9 @@ export default class SubscriptionPlanBuilder extends SubscriptionInlinePlanBuild
             throw new PaypalTSError("Invalid description the length must be less than 127 characters.");
         if (this.quantity_supported && typeof this.quantity_supported !== "boolean")
             throw new PaypalTSError("Invalid quantity_supported the quantity_supported must be a boolean.");
+        if (this.taxes) {
+            if (!this.taxes.percentage || !/^((-?[0-9]+)|(-?([0-9]+)?[.][0-9]+))$/.test(this.taxes.percentage.toString()))
+                throw new PaypalTSError("Invalid taxes.percentage the taxes.percentage must be a number.");
+        }
     }
 }
